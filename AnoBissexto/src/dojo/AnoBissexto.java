15c04d322dc11f3333b692230a44562c3dfcbbbc
Aluno <Aluno@L4007>
2017-09-04 09:24:15 -0300

    Criado o método anoValido

--- AnoBissexto/src/dojo/AnoBissexto.java
+++ AnoBissexto/src/dojo/AnoBissexto.java
@@ -2,6 +2,13 @@ package dojo;
 
 public class AnoBissexto {
 
-	
+	public static boolean anoValido(int ano) {
+		if (ano % 4 == 0 || ano % 400 == 0) {
+			return true;
+		} else if (ano % 100 != 0){
+			return false;
+		}
+		return false;
+	}
 	
 }
