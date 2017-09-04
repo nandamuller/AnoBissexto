205106f8db956303a4b67ad900363a1296b80c8f
Aluno <Aluno@L4007>
2017-09-04 09:25:29 -0300

    Criado os testes com vários anos, PROBLEMA RESOLVIDO.

--- AnoBissexto/src/dojo/AnoBissextoTest.java
+++ AnoBissexto/src/dojo/AnoBissextoTest.java
@@ -10,5 +10,48 @@ public class AnoBissextoTest {
 		boolean ano = AnoBissexto.anoValido(2001); 
 		assertEquals(false, ano);
 	}
+	
+	@Test
+	public void testAno1600() {
+		boolean ano = AnoBissexto.anoValido(1600); 
+		assertEquals(true, ano);
+	}
+	
+	@Test
+	public void testAno1732() {
+		boolean ano = AnoBissexto.anoValido(1732); 
+		assertEquals(true, ano);
+	}
+	
+	@Test
+	public void testAno2008() {
+		boolean ano = AnoBissexto.anoValido(2008); 
+		assertEquals(true, ano);
+	}
+	
+	@Test
+	public void testAno1944() {
+		boolean ano = AnoBissexto.anoValido(1944); 
+		assertEquals(true, ano);
+	}
+	
+	@Test
+	public void testAno1889() {
+		boolean ano = AnoBissexto.anoValido(1889); 
+		assertEquals(false, ano);
+	}
+	
+	@Test
+	public void testAno1951() {
+		boolean ano = AnoBissexto.anoValido(1951); 
+		assertEquals(false, ano);
+	}
+	
+	@Test
+	public void testAno2011() {
+		boolean ano = AnoBissexto.anoValido(2011); 
+		assertEquals(false, ano);
+	}
+
 
 }
